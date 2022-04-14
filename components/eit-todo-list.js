ac552c7bb45ecfe2a1d92b4d17617c5693ace50c
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2022-04-14 15:37:49 +0200

    repeticiones en templates

--- components/eit-todo-list.js
+++ components/eit-todo-list.js
@@ -17,16 +17,42 @@ export class EitTodoList extends LitElement {
                 background-color: #eee;
                 padding: 1rem;
             }
+            ul {
+                margin: 1rem 0;
+                padding: 0;
+            }
+            li {
+                display: flex;
+                align-items: center;
+                list-style-type: none;
+                margin-bottom: 0.8rem;
+            }
+            li span {
+                margin-left: 0.5rem;
+            }
         `
     ];
 
     static properties = {
-        completed: { type: Boolean },
+        todos: { type: Array },
     }
 
     constructor() {
         super();
-        this.completed = false;
+        this.todos = [
+            { 
+                title: 'Comprar en el super',
+                completed: false,
+            },
+            { 
+                title: 'Llamar a mamá',
+                completed: false,
+            },
+            { 
+                title: 'Descongelar la lasaña',
+                completed: true,
+            },
+        ];
     }
 
     render() {
@@ -46,17 +72,18 @@ export class EitTodoList extends LitElement {
 
     get bodyTemplate() {
         return html`
-        <div>
-            ${this.completed
-                ? icons.done
-                : icons.fiber_manual_record
-            }
-        </div>
+        <ul>
+            ${this.todos.map( (todo) => html`
+                <li>
+                    ${todo.completed
+                        ? icons.done
+                        : icons.fiber_manual_record
+                    }
+                    <span>${todo.title}</span>
+                </li>
+            ` )}
+        </ul>
         `
-    }
-
-    changeCompleted() {
-        this.completed = !this.completed;
     }
 }
 customElements.define('eit-todo-list', EitTodoList);
